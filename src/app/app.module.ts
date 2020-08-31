6b2d8f3b970069d8f454df4781938f4fa09f7811
Alvis F <falvis1999@gmail.com>
2020-08-31 11:30:55 +0530

    created model deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { FormQuoteComponent } from './form-quote/form-quote.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
